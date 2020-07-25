d172aa70333ba972e568015872cd1a6d029060ad
TAKEHARA Shunpei <takehara-s@gnavi.co.jp>
2020-07-26 01:46:07 +0900

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const prefix = process.env.SITE_PREFIX
+
 module.exports = {
   /* Your site config here */
   plugins: ['gatsby-plugin-styled-components'],
-  assetPrefix: '/time-quiz/dist/',
+  pathPrefix: prefix,
 }
